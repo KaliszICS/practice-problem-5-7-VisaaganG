55fe8eb45078079f8d2d077da31db03673c66b3c
VisaaganG <343505582@tdsb.ca>
2026-06-17 03:39:37 +0000

    Questions Finished

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,31 @@
+import java.util.ArrayList;
+
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+	public static void swap(ArrayList<String> list, int index1, int index2) {
+        String temp = list.get(index1);
+        list.set(index1, list.get(index2));
+        list.set(index2, temp);
+    }
 
-	}
+	public static ArrayList<Double> createArrayList(double[] arr) {
+        ArrayList<Double> list = new ArrayList<>();
+        for (double value : arr) {
+            list.add(value);
+        }
+        return list;
+    }
+	public static ArrayList<Integer> mergeLists(ArrayList<Integer> list1, ArrayList<Integer> list2) {
+        ArrayList<Integer> together = new ArrayList<>();
+        for (int num : list1) {
+            together.add(num);
+        }
+        for (int num : list2) {
+            together.add(num);
+        }
+        return together;
+    }
+
+
 
 }
